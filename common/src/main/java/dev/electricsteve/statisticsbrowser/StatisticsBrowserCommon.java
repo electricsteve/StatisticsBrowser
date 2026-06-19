25c5795e718a3aba34e021eed1861403871ca3a3
electricsteve <96793824+electricsteve@users.noreply.github.com>
2026-06-20 01:52:33 +0200

    thx mycelium mod network people, specifically mysticdrew

--- common/src/main/java/dev/electricsteve/statisticsbrowser/StatisticsBrowserCommon.java
+++ common/src/main/java/dev/electricsteve/statisticsbrowser/StatisticsBrowserCommon.java
@@ -1,9 +1,18 @@
 package dev.electricsteve.statisticsbrowser;
 
 import dev.electricsteve.statisticsbrowser.platform.Services;
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 
 public class StatisticsBrowserCommon {
     public static void init() {
         Constants.LOG.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add(staticFiles -> {
+                staticFiles.directory = "/web";
+                staticFiles.location = Location.CLASSPATH;
+            });
+            config.routes.get("/api/status", ctx -> ctx.json("test"));
+        }).start(7070);
     }
 }
